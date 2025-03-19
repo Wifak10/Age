a82731eaad440a2668c80890c4e070d902c41074
wifak10 <wifakayekpola123@gmail.com>
2025-03-19 10:50:40 +0100

    Logique de l'implémentation du calcul d'âge

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ function calculateAge(){
     const birthdate = new Date(birthdateInput) ;
     const today = new Date('2025-03-19');
     let age = today.getFullYear() - birthdate.getFullYear();
+    const monthDiff = today.getMonth() - birthdate.getMonth() ;
+    const dayDiff = today.getDate() - birthdate.getDate();
+
+    //Ajuster l'âge si l'anniverssaire n'est pas encore passé cette année
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age-- ;
+    }
+    //Calculer les jours vécus (approximation)
+    const daysLived = Math.floor((today - birthdate) / (1000 * 60 * 60 * 24));
 }
